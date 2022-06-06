1a2af1a048c55d9b1563955a80b6f9fd6bc233e9
Nadzeya Sivukha (CODECOOL POLAND SP. Z O.O.) <WGK867@motorolasolutions.com>
2022-06-06 13:09:36 +0200

    Song and User entities added

---
+++ SimpleSongs.Data/Entities/Song.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSongs.Data.Entities
+{
+    public class Song
+    {
+    }
+}
