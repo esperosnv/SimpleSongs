a641274e0ac3bfdaf26e9d21faf820ac3e19b8e7
Nadzeya Sivukha (CODECOOL POLAND SP. Z O.O.) <WGK867@motorolasolutions.com>
2022-06-06 13:35:53 +0200

    Created SongsContext and Migrations

---
+++ SimpleSongs.Data/Context/SongsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimpleSongs.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace SimpleSongs.Data.Context
+{
+    public class SongsContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=SimpleSongs;Integrated Security=True");
+        }
+    }
+}
--- SimpleSongs.Data/Entities/Song.cs
+++ SimpleSongs.Data/Entities/Song.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleSongs.Data.Entities
 {
     public class Song
     {
+        public Guid SongId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string AlbumName { get; set; }
+        public double Length { get; set; }
+
     }
 }
