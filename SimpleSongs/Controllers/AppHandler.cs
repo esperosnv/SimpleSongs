e734795f4ddc00484b54e3bffe58254325282aeb
Nadzeya Sivukha (CODECOOL POLAND SP. Z O.O.) <WGK867@motorolasolutions.com>
2022-06-06 12:35:26 +0200

    Created empty AppHandler

---
+++ SimpleSongs/Controllers/AppHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SimpleSongs.Controllers
+{
+    public class AppHandler
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
--- SimpleSongs/Program.cs
+++ SimpleSongs/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SimpleSongs.Controllers;
+
+namespace SimpleSongs
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            AppHandler appHandler = new();
+            appHandler.Run();
+        }
+    }
+}
