ec8b9701e32005f8fe406bb46dd7e96ddfb0c327
Nadzeya Sivukha (CODECOOL POLAND SP. Z O.O.) <WGK867@motorolasolutions.com>
2022-06-06 13:45:40 +0200

    Implemented SongRepository and added IBaseRepository

---
+++ SimpleSongs.Data/DAL/SongRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimpleSongs.Data.Context;
+using SimpleSongs.Data.Entities;
+
+namespace SimpleSongs.Data.DAL
+{
+    public class SongRepository : IBaseRepository<Song>
+    {
+
+        private SongsContext _songsContext;
+        public SongRepository()
+        {
+            _songsContext = new SongsContext();
+        }
+        public void Add(Song entity)
+        {
+            _songsContext.Songs.Add(entity);
+        }
+
+        public void Delete(Song entity)
+        {
+            _songsContext.Songs.Remove(entity);
+        }
+
+        public void Edit(Song entity)
+        {
+            _songsContext.Songs.Update(entity);
+        }
+
+        public List<Song> GetAll()
+        {
+            return _songsContext.Songs.ToList();
+        }
+
+        public Song GetSingle(Func<Song, bool> condition)
+        {
+            return _songsContext.Songs.First();
+        }
+
+        public void Save()
+        {
+            _songsContext.SaveChanges();
+        }
+    }
+}
