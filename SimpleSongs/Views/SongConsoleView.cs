498cfd996e9a2e8e9d2a44f6c8b5d253fc19c485
Nadzeya Sivukha (CODECOOL POLAND SP. Z O.O.) <WGK867@motorolasolutions.com>
2022-06-06 14:15:50 +0200

    Created SongConsoleView

---
+++ SimpleSongs/Views/SongConsoleView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SimpleSongs.Views.Interfaces;
+using SimpleSongs.Data.Entities;
+
+namespace SimpleSongs.Views
+{
+    public class SongConsoleView : IView<Song>
+    {
+        public void DisplayAll(List<Song> entities)
+        {
+            entities.ForEach(DisplaySingle);
+        }
+
+        public void DisplaySingle(Song entity)
+        {
+            Console.WriteLine($"Song title: {entity.Title}, Author: {entity.Author}, AlbumName: {entity.AlbumName}, Length: {entity.Length}");
+        }
+    }
+}
